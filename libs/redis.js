b43f40a19c641962febf91593c00553466015330
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-25 17:36:29 +0100

    Redis for value storage

--- libs/redis.js
+++ libs/redis.js
@@ -1,1 +1,9 @@
 const { redisClient } = require("../configs/redis");
+4;
+async function addToRedis(key, value, expiresIn = 60 * 60 * 24) {
+  try {
+    return await redisClient.set(key, value, "EX", expiresIn);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
