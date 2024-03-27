1173758c26e9574f759bc25aefda9a497aab3c86
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-27 20:19:54 +0100

    Email config set up

--- utils/emailConfig.js
+++ utils/emailConfig.js
@@ -4,13 +4,24 @@ const emailAccess = process.env.SMTP_PASSWORD;
 const SMTP_HOST = "smtp.gmail.com";
 const SMTP_PORT = 465;
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  name: SMTP_NAME,
-  port: SMTP_PORT,
-  secure: true,
-  auth: {
-    user: sender,
-    pass: emailAccess,
-  },
-});
+const sendEmail = async ({ to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    name: SMTP_NAME,
+    port: SMTP_PORT,
+    secure: true,
+    auth: {
+      user: sender,
+      pass: emailAccess,
+    },
+  });
+
+  return await transporter.sendMail({
+    from: sender,
+    to,
+    subject,
+    html,
+  });
+};
+
+module.exports = { sendEmail };
