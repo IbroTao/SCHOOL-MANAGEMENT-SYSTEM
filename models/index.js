83ac2d409ad433ae0f9e9216430608fac4655427
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-28 06:00:36 +0100

    Index file for models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,11 @@
-module.exports.Teacher = require("./teacher.model");
-module.exports.User = require("./user.model");
-module.exports.Admin = require("./admin.model");
-module.exports.Assignment = require("./assignment.model");
-module.exports.Blog = require("./blog.model");
+module.exports.Teachers = require("./teacher.model");
+module.exports.Users = require("./user.model");
+module.exports.Admins = require("./admin.model");
+module.exports.Assignments = require("./assignment.model");
+module.exports.Blogs = require("./blog.model");
+module.exports.Chats = require("./chat.model");
+module.exports.NoticeModel = require("./notice.model");
+module.exports.Posts = require("./post.model");
+module.exports.Drafts = require("./drafts.model");
+module.exports.Contacts = require("./contact.model");
+module.exports.Messages = require("./message.model");
