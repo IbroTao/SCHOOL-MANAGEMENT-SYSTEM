d8dea3925725ca06bf52983cf765d2b1d1442c87
IbroTao <ibrahimtaofeek005@gmail.com>
2024-03-31 13:24:45 +0100

    Token model set up

--- models/token.model.js
+++ models/token.model.js
@@ -1,14 +1,30 @@
 const mongoose = require("mongoose");
 
-const tokenSchema = new mongoose.Schema({
-  token: {
-    type: String,
-    required: true,
-    index: true,
+const tokenSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    type: {
+      type: String,
+      enum: ["access", "refresh"],
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+    blacklisted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  type: {
-    type: String,
-    enum: ["access", "refresh"],
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const Token = mongoose.model("tokens", tokenSchema);
+module.exports = { Token };
